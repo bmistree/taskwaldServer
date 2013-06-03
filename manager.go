9d7003ae9f768eae35f1b2b315f03b9be96acd27
bmistree <bmistree@stanford.edu>
2013-06-03 14:47:43 -0700

    Separated manager out into own separate file within server's overarching package.

---
+++ manager.go
@@ -0,0 +1,38 @@
+package main
+
+import "log"
+
+
+/***************** MANAGER DEFINITIONS ********/
+
+type Manager struct {
+	// map [<key type>] <value type>
+	all_connections map[uint32] *Player
+	register_channel chan *Player
+	unregister_channel chan *Player
+}
+
+/*
+ Sends a ping message to all connected clients
+*/
+func (man *Manager) broadcast_ping() {
+	for _, value := range man.all_connections {
+		value.ping()
+	}
+}
+
+func (man *Manager) manager_loop() {
+	// infinite loop waiting on additional work
+	for {
+		select {
+		case player_to_register := <- man.register_channel:
+			log.Println("Registering new player")
+			man.all_connections[player_to_register.id] = player_to_register
+			man.broadcast_ping()
+		case player_to_unregister := <- man.unregister_channel:
+			log.Println("Removing player")
+			delete (man.all_connections, player_to_unregister.id)
+			close (player_to_unregister.msg_output_queue)
+		}
+	}
+}
--- server.go
+++ server.go
@@ -6,41 +6,6 @@ import "net/http"
 var LISTENING_ADDR = "127.0.0.1:18080"
 
 
-/***************** MANAGER DEFINITIONS ********/
-
-type Manager struct {
-	// map [<key type>] <value type>
-	all_connections map[uint32] *Player
-	register_channel chan *Player
-	unregister_channel chan *Player
-}
-
-/*
- Sends a ping message to all connected clients
-*/
-func (man *Manager) broadcast_ping() {
-	for _, value := range man.all_connections {
-		value.ping()
-	}
-}
-
-func (man *Manager) manager_loop() {
-	// infinite loop waiting on additional work
-	for {
-		select {
-		case player_to_register := <- man.register_channel:
-			log.Println("Registering new player")
-			man.all_connections[player_to_register.id] = player_to_register
-			man.broadcast_ping()
-		case player_to_unregister := <- man.unregister_channel:
-			log.Println("Removing player")
-			delete (man.all_connections, player_to_unregister.id)
-			close (player_to_unregister.msg_output_queue)
-		}
-	}
-}
-
-
 // FIXME: want this to be atomic
 var id_counter uint32 = 0
 
